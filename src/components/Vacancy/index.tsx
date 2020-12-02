bd4e2eeb8842612ae7b631d01f88fb13f0bc70dc
Lucas Emanuel <lucasemanuelss@hotmail.com>
2020-12-02 14:52:29 -0300

    fix: Dependencies and linting

--- src/components/Vacancy/index.tsx
+++ src/components/Vacancy/index.tsx
@@ -4,52 +4,52 @@ import React, {
   useState,
   useCallback,
   useEffect,
-  OptionHTMLAttributes,
-} from "react";
-import Input from "../Input";
-import Textarea from "../Textarea";
-import Select from "../Select";
-import ToggleSwitch from "../ToggleSwitch";
-import Button from "../Button";
-import { BodyVacancy } from "./styles";
-import { inputChange } from "../../utils/inputChange";
-import { selectChange } from "../../utils/selectChange";
-import { textareaChange } from "../../utils/textareaChange";
-import { finalYearOptions, yearOptions } from "../../utils/dates";
-import { AxiosError } from "axios";
-import api from "../../services/api";
-import edit from "../../assets/icon/editar.svg";
-import trash from "../../assets/icon/lixeira.svg";
-import Modal from "../Modal";
-import { AreaType } from "../../components/SelectArea";
-import { ToolType } from "../../components/SelectTools";
-import { createOptionAreas, createOptionTools } from "../../utils/projects";
+  OptionHTMLAttributes
+} from 'react'
+import Input from '../Input'
+import Textarea from '../Textarea'
+import Select from '../Select'
+import ToggleSwitch from '../ToggleSwitch'
+import Button from '../Button'
+import { BodyVacancy } from './styles'
+import { inputChange } from '../../utils/inputChange'
+import { selectChange } from '../../utils/selectChange'
+import { textareaChange } from '../../utils/textareaChange'
+import { finalYearOptions, yearOptions } from '../../utils/dates'
+import { AxiosError } from 'axios'
+import api from '../../services/api'
+import edit from '../../assets/icon/editar.svg'
+import trash from '../../assets/icon/lixeira.svg'
+import Modal from '../Modal'
+import { AreaType } from '../../components/SelectArea'
+import { ToolType } from '../../components/SelectTools'
+import { createOptionAreas, createOptionTools } from '../../utils/projects'
 
 interface VacanciesType {
-  nome: string;
-  descricao: string;
-  visibilidade: true;
-  objetivo: string;
-  foto_capa: string;
-  id: number;
+  nome: string
+  descricao: string
+  visibilidade: true
+  objetivo: string
+  foto_capa: string
+  id: number
 }
 interface ProjectType {
-  nome: string;
-  descricao: string;
-  visibilidade: true;
-  objetivo: string;
-  foto_capa: string;
-  areas: AreaType[];
-  habilidades: ToolType[];
-  id: number;
+  nome: string
+  descricao: string
+  visibilidade: true
+  objetivo: string
+  foto_capa: string
+  areas: AreaType[]
+  habilidades: ToolType[]
+  id: number
 }
 interface VacancyProps {
-  project: ProjectType;
+  project: ProjectType
 }
 
 const Vacancy: React.FC<VacancyProps> = ({ project }) => {
-  const [showRegister, setShowRegister] = useState<boolean>(false);
-  const [editingId, setEditingId] = useState<number>(0);
+  const [showRegister, setShowRegister] = useState<boolean>(false)
+  const [editingId, setEditingId] = useState<number>(0)
   const optionsContrato: Array<OptionHTMLAttributes<HTMLOptionElement>> = [
     { value: 'trainee', label: 'Trainee' },
     { value: 'terceirizado', label: 'Terceirizado' },
@@ -62,210 +62,112 @@ const Vacancy: React.FC<VacancyProps> = ({ project }) => {
     { value: 'meioPeríodo', label: 'Meio Período' },
     { value: 'tempoIntegral', label: ' Tempo Integral' }
   ]
-  
-  const optionsAreas: Array<OptionHTMLAttributes<HTMLOptionElement>> = createOptionAreas(project.areas);
-  const optionsTools: Array<OptionHTMLAttributes<HTMLOptionElement>> = createOptionTools(project.habilidades);
+
+  const optionsAreas: Array<
+    OptionHTMLAttributes<HTMLOptionElement>
+  > = createOptionAreas(project.areas)
+  const optionsTools: Array<
+    OptionHTMLAttributes<HTMLOptionElement>
+  > = createOptionTools(project.habilidades)
   useEffect(() => {
     api
-      .get("/api/v1/experiencias/academica/me", {
-        withCredentials: true,
+      .get('/api/v1/experiencias/academica/me', {
+        withCredentials: true
       })
-      .then((response) => {
-
-      })
+      .then(response => {})
       .catch((err: AxiosError) => {
         // Returns error message from backend
-        return err?.response?.data.detail;
-      });
-  }, [editingId, showRegister]);
+        return err?.response?.data.detail
+      })
+  }, [editingId, showRegister])
 
   return (
     <BodyVacancy>
-
-
       <h2>Vagas</h2>
       {!showRegister ? (
         <div className="vagas">
           <div key={1} className="vaga-cadastrada">
-
             <section className="icones">
-              <img
-                src={edit}
-                alt="editar vaga"
-
-              />
-              <img
-                src={trash}
-                alt="apagar vaga"
-
-              />
-
+              <img src={edit} alt="editar vaga" />
+              <img src={trash} alt="apagar vaga" />
             </section>
             <fieldset className="info-vagas">
-
-
               <legend>Ux Designer</legend>
               <p>
-                Trainee | Não remunerado <br />
-                  2 Vagas
-                </p>
-
+                Trainee | Não remunerado <br />2 Vagas
+              </p>
             </fieldset>
           </div>
           <div key={1} className="vaga-cadastrada">
-
             <section className="icones">
-              <img
-                src={edit}
-                alt="editar vaga"
-
-              />
-              <img
-                src={trash}
-                alt="apagar vaga"
-
-              />
-
+              <img src={edit} alt="editar vaga" />
+              <img src={trash} alt="apagar vaga" />
             </section>
             <fieldset className="info-vagas">
-
-
               <legend>Ux Designer</legend>
               <p>
-                Trainee | Não remunerado <br />
-                  2 Vagas
-                </p>
-
+                Trainee | Não remunerado <br />2 Vagas
+              </p>
             </fieldset>
           </div>
           <div key={1} className="vaga-cadastrada">
-
             <section className="icones">
-              <img
-                src={edit}
-                alt="editar vaga"
-
-              />
-              <img
-                src={trash}
-                alt="apagar vaga"
-
-              />
-
+              <img src={edit} alt="editar vaga" />
+              <img src={trash} alt="apagar vaga" />
             </section>
             <fieldset className="info-vagas">
-
-
               <legend>Ux Designer</legend>
               <p>
-                Trainee | Não remunerado <br />
-                  2 Vagas
-                </p>
-
+                Trainee | Não remunerado <br />2 Vagas
+              </p>
             </fieldset>
           </div>
           <div key={1} className="vaga-cadastrada">
-
             <section className="icones">
-              <img
-                src={edit}
-                alt="editar vaga"
-
-              />
-              <img
-                src={trash}
-                alt="apagar vaga"
-
-              />
-
+              <img src={edit} alt="editar vaga" />
+              <img src={trash} alt="apagar vaga" />
             </section>
             <fieldset className="info-vagas">
-
-
               <legend>Ux Designer</legend>
               <p>
-                Trainee | Não remunerado <br />
-                  2 Vagas
-                </p>
-
+                Trainee | Não remunerado <br />2 Vagas
+              </p>
             </fieldset>
           </div>
           <div key={1} className="vaga-cadastrada">
-
             <section className="icones">
-              <img
-                src={edit}
-                alt="editar vaga"
-
-              />
-              <img
-                src={trash}
-                alt="apagar vaga"
-
-              />
-
+              <img src={edit} alt="editar vaga" />
+              <img src={trash} alt="apagar vaga" />
             </section>
             <fieldset className="info-vagas">
-
-
               <legend>Ux Designer</legend>
               <p>
-                Trainee | Não remunerado <br />
-                  2 Vagas
-                </p>
-
+                Trainee | Não remunerado <br />2 Vagas
+              </p>
             </fieldset>
           </div>
           <div key={1} className="vaga-cadastrada">
-
             <section className="icones">
-              <img
-                src={edit}
-                alt="editar vaga"
-
-              />
-              <img
-                src={trash}
-                alt="apagar vaga"
-
-              />
-
+              <img src={edit} alt="editar vaga" />
+              <img src={trash} alt="apagar vaga" />
             </section>
             <fieldset className="info-vagas">
-
-
               <legend>Ux Designer</legend>
               <p>
-                Trainee | Não remunerado <br />
-                  2 Vagas
-                </p>
-
+                Trainee | Não remunerado <br />2 Vagas
+              </p>
             </fieldset>
           </div>
           <div key={1} className="vaga-cadastrada">
-
             <section className="icones">
-              <img
-                src={edit}
-                alt="editar vaga"
-
-              />
-              <img
-                src={trash}
-                alt="apagar vaga"
-
-              />
-
+              <img src={edit} alt="editar vaga" />
+              <img src={trash} alt="apagar vaga" />
             </section>
             <fieldset className="info-vagas">
-
-
               <legend>Ux Designer</legend>
               <p>
-                Trainee | Não remunerado <br />
-                  2 Vagas
-                </p>
-
+                Trainee | Não remunerado <br />2 Vagas
+              </p>
             </fieldset>
           </div>
           <button onClick={() => setShowRegister(true)}>
@@ -274,95 +176,86 @@ const Vacancy: React.FC<VacancyProps> = ({ project }) => {
           </button>
         </div>
       ) : (
-          <form
-            className="form-vaga"
-          >
-            <Input
-              label="Cargo"
-              name="cargo"
-              required
+        <form className="form-vaga">
+          <Input
+            label="Cargo"
+            name="cargo"
+            required
             //defaultValue={academicFormData?.instituicao}
-            />
-            <Input
-              label="Perfil"
-              name="perfil"
-              required
+          />
+          <Input
+            label="Perfil"
+            name="perfil"
+            required
             //defaultValue={academicFormData?.instituicao}
-            />
-            <Input
-              label="Quantidade"
-              name="quantidade"
-              type="number"
-              required
+          />
+          <Input
+            label="Quantidade"
+            name="quantidade"
+            type="number"
+            required
             //defaultValue={academicFormData?.instituicao}
-            />
-            <div className="bloco-area">
-
-              <Select
-                label="Habilidade ou Ferramentas"
-                name="habilidade"
-                options={optionsTools}
-                isMulti
-              />
-            </div>
+          />
+          <div className="bloco-area">
             <Select
-              label="Áreas"
-              name="areas"
-              options={optionsAreas}
+              label="Habilidade ou Ferramentas"
+              name="habilidade"
+              options={optionsTools}
               isMulti
             />
+          </div>
+          <Select
+            label="Áreas"
+            name="areas"
+            options={optionsAreas}
+            isMulti
+          />
 
-
-            <Textarea
-              name="descricao"
-              label="Descrição"
-              required
+          <Textarea
+            name="descricao"
+            label="Descrição"
+            required
             //defaultValue={academicFormData?.descricao}
+          />
+          <section className="bloco-contrato">
+            <Select
+              label="Tipo de contrato"
+              options={optionsContrato}
+              name="tipoContrato"
             />
-            <section className="bloco-contrato">
-              <Select
-                label="Tipo de contrato"
-                options={optionsContrato}
-                name="tipoContrato"
-              />
-              <ToggleSwitch
-                label="Remunerado"
-                name="remunerado"
-                id="remunerado"
+            <ToggleSwitch
+              label="Remunerado"
+              name="remunerado"
+              id="remunerado"
               //defaultChecked={
               //  academicFormData &&
               //  academicFormData?.situacao === "Incompleto"
               //}
-              />
-            </section>
-            <section className="area-botoes">
-              <Button
-                type="submit"
-                theme="primary-green"
+            />
+          </section>
+          <section className="area-botoes">
+            <Button
+              type="submit"
+              theme="primary-green"
               //disabled={academicFormData === {} as AcademicType? false:true}
-              >
-                Salvar
-              </Button>
-              <Button
-                theme="secondary-green"
-              >
-                Excluir
-              </Button>
-              <Button
-                onClick={() => {
-                  setShowRegister(false);
-                  setEditingId(0);
-                  //setAcademicFormData(initialAcademicData);
-                }}
-              >
-                Cancelar
-              </Button>
-            </section>
-
-          </form>
-        )}
+            >
+              Salvar
+            </Button>
+            <Button theme="secondary-green">Excluir</Button>
+            <Button
+              onClick={() => {
+                setShowRegister(false)
+                setEditingId(0)
+                //setAcademicFormData(initialAcademicData);
+              }}
+            >
+              Cancelar
+            </Button>
+          </section>
+        </form>
+      )}
     </BodyVacancy>
-  );
-};
+  )
+}
 
-export default Vacancy;
+export default Vacancy
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
+import React from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import ProfileFeatures from './pages/ProfileFeatures'
-import ExperienceAreas from './pages/ExperienceAreas';
-import MasteryTools from './pages/MasteryTools';
-import CreateProject from './pages/CreateProject';
-import ApproveProject from './pages/ApproveProject';
-import Projects from './pages/Projects';
+import ExperienceAreas from './pages/ExperienceAreas'
+import MasteryTools from './pages/MasteryTools'
+import CreateProject from './pages/CreateProject'
+import ApproveProject from './pages/ApproveProject'
+import Projects from './pages/Projects'
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext'
 
 function Routes() {
   return (
@@ -18,14 +18,13 @@ function Routes() {
         <Route exact path="/" component={Home} />
         <Route path="/signup/:step" component={SignUp} />
         <Route path="/profilefeatures" component={ProfileFeatures} />
-        <Route path="/experienceareas" component={ ExperienceAreas} />
-        <Route path="/masterytools" component={ MasteryTools} />
-        <Route path="/createproject" component={ CreateProject} />
-        <Route path="/approveproject" component={ ApproveProject} />
-        <Route path="/projects/:id" component={ Projects} />
+        <Route path="/experienceareas" component={ExperienceAreas} />
+        <Route path="/masterytools" component={MasteryTools} />
+        <Route path="/createproject" component={CreateProject} />
+        <Route path="/approveproject" component={ApproveProject} />
+        <Route path="/projects/:id" component={Projects} />
       </BrowserRouter>
     </AuthProvider>
   )
-
 }
-export default Routes;
+export default Routes
